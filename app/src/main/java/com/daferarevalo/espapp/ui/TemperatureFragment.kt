81024c2027ebe410f7f7dab10e04bf30ae3199e6
Dario Arevalo <daferd19@gmail.com>
2021-01-22 08:58:45 -0500

    Implentacion de grafico de barras

--- app/src/main/java/com/daferarevalo/espapp/ui/TemperatureFragment.kt
+++ app/src/main/java/com/daferarevalo/espapp/ui/TemperatureFragment.kt
@@ -7,14 +7,21 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.daferarevalo.espapp.R
 import com.daferarevalo.espapp.databinding.FragmentTemperatureBinding
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
 
 class TemperatureFragment : Fragment() {
 
     private lateinit var binding: FragmentTemperatureBinding
 
+    companion object {
+        private val TAG = "TemperatureFragment"
+    }
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_temperature, container, false)
@@ -24,6 +31,33 @@ class TemperatureFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding = FragmentTemperatureBinding.bind(view)
+
+        //binding.lineChart.onChartGestureListener
+        //binding.lineChart.setOnChartValueSelectedListener(this@TemperatureFragment)
+
+
+        val yValues = ArrayList<BarEntry>()
+
+        yValues.add(BarEntry(8f, 0f))
+        yValues.add(BarEntry(2f, 1f))
+        yValues.add(BarEntry(5f, 2f))
+        yValues.add(BarEntry(20f, 3f))
+        yValues.add(BarEntry(15f, 4f))
+        yValues.add(BarEntry(19f, 5f))
+
+        val barDataSet = BarDataSet(yValues, "Cells")
+
+        val labels = ArrayList<String>()
+        labels.add("18-Jan")
+        labels.add("19-Jan")
+        labels.add("20-Jan")
+        labels.add("21-Jan")
+        labels.add("22-Jan")
+        labels.add("23-Jan")
+
+        val data = BarData(barDataSet)
+        binding.barChart.data = data
+
 
     }
 }
